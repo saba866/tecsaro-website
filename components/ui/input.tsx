104abf7e675a036f2418be1b5efdf65d03fa060f
Saba Begum <begumsaba430@gmail.com>
2025-08-02 16:10:41 +0530

    Added input.tsx and textarea.tsx components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
@@ -17,6 +16,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     )
   }
 )
+
 Input.displayName = "Input"
 
 export { Input }
