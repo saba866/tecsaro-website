5ae29159c8daf262f04df18ad3b192208b828f4b
Saba Begum <begumsaba430@gmail.com>
2025-08-02 18:40:04 +0530

    Fix alias issue for Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,3 +22,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
